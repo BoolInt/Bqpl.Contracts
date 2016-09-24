9aba2f414fe9d814d20c5e2358a0b17b24fee0f4
BoolInt <atc_tyrann@hotmail.com>
2016-09-24 21:28:22 +0200

    Test

---
+++ Bqpl.Contracts/Argument.cs
@@ -0,0 +1,33 @@
+﻿using Bqpl.Exceptions;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace Bqpl.Contracts
+{
+  public static class Argument
+  {
+    [Pure]
+    [ContractArgumentValidator]
+    [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void NotNull<T>(string argumentName, [ValidatedNotNull]T value)
+      where T : class
+    {
+      if (value == null)
+        throw ExceptionActivator.CreateArgumentNullException(argumentName);
+      Contract.EndContractBlock();
+    }
+
+    [Pure]
+    [ContractArgumentValidator]
+    [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void NotNullOrEmpty(string argumentName, [ValidatedNotNull]string value)
+    {
+      if (string.IsNullOrEmpty(value))
+        throw ExceptionActivator.CreateArgumentException(argumentName);
+      Contract.EndContractBlock();
+    }
+  }
+}
---
+++ Bqpl.Contracts/ValidatedNotNullAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Bqpl.Contracts
+{
+  [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+  public sealed class ValidatedNotNullAttribute : Attribute
+  {
+  }
+}
