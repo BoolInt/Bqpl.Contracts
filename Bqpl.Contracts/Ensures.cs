c8165c5073d5ebaa0910c778fb26581e8e42016b
BoolInt <atc_tyrann@hotmail.com>
2016-09-25 12:08:24 +0200

    x

---
+++ Bqpl.Contracts/Ensures.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace Bqpl.Contracts
+{
+  public static class Ensures
+  {
+    [Pure]
+    [ContractAbbreviator]
+    [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void NotNull<T>(T value)
+      where T : class
+    {
+      Contract.Ensures(value != null);
+    }
+
+    [Pure]
+    [ContractAbbreviator]
+    [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void NotNull<T>(T? value)
+      where T : struct
+    {
+      Contract.Ensures(value != null);
+    }
+  }
+}
---
+++ Bqpl.Contracts/Result.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace Bqpl.Contracts
+{
+  public static class Result
+  {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
+    [Pure]
+    [ContractAbbreviator]
+    [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void NotNull<T>()
+      where T : class
+    {
+      Ensures.NotNull(Contract.Result<T>());
+    }
+  }
+}
